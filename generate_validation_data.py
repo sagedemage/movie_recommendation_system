8a8c168915e32befc56a97c7f5a765b458132e2e
Salmaan Saeed <salsaeed999@gmail.com>
2024-10-12 18:33:07 -0400

    created a script to generate a validation data depending on which movie is picked

---
+++ generate_validation_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+csv_dataset_file = 'data/imdb_top_1000.csv'
+
+VALIDATION_DATA_DIR = 'validation_data/'
+
+csv_validation_dataset = VALIDATION_DATA_DIR + 'favorite_movies_imdb.csv'
+
+write_data = {
+}
+
+df_data = pd.read_csv(csv_dataset_file)
+
+# Pick a movie
+row = df_data.iloc[10]
+genre = row[4]
+pick_genre_list = genre.split(",")
+
+file = open(VALIDATION_DATA_DIR + 'picked_genre_list.txt', 'w')
+file.write(str(pick_genre_list))
+
+print(pick_genre_list)
+print("")
+
+columns = df_data.columns
+
+for i in range(len(columns)):
+    column = columns[i]
+    write_data[column] = []
+
+for i, row in df_data.iterrows():
+    genre = row[4]
+    genre_list = genre.split(",")
+
+    for j in range(len(genre_list)):
+        if genre_list[j] in pick_genre_list:
+            for k in range(len(columns)):
+                column = columns[k]
+                write_data[column].append(row[column])
+
+df_write_data = pd.DataFrame(write_data)
+df_write_data.to_csv(csv_validation_dataset, index=False)
