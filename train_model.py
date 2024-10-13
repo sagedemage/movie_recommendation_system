40d7aeae26b5f1041316f0e70e62f86afddd6ca0
Salmaan Saeed <salsaeed999@gmail.com>
2024-10-13 03:31:46 -0400

    created a main function

--- train_model.py
+++ train_model.py
@@ -1,5 +1,8 @@
 import torch
 from torch.utils.data import DataLoader
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
+
 from datetime import datetime
 import math
 
@@ -14,7 +17,7 @@ csv_dataset = 'data/imdb_top_1000.csv'
 csv_validation_dataset = 'validation_data/favorite_movies_imdb.csv'
 BATCH_SIZE = 64
 
-def train_one_epoch(epoch_index, tb_writer: torch.utils.tensorboard.writer.SummaryWriter):
+def train_one_epoch(epoch_index, tb_writer: SummaryWriter, training_loader: DataLoader, optimizer: SGD, device: str, model: NeuralNetwork, loss_fn: CrossEntropyLoss):
     running_loss = 0.
     last_loss = 0.
 
@@ -51,100 +54,104 @@ def train_one_epoch(epoch_index, tb_writer: torch.utils.tensorboard.writer.Summa
 
         return last_loss
 
-training_set = CustomDataset(csv_dataset)
-validation_set = CustomDataset(csv_validation_dataset)
+def main():
+    training_set = CustomDataset(csv_dataset)
+    validation_set = CustomDataset(csv_validation_dataset)
 
-print("Length: " + str(training_set.__len__()))
-print("")
+    print("Length: " + str(training_set.__len__()))
+    print("")
 
-movie_id, title, released_year, runtime, genre, imdb_rating, director = training_set.__getitem__(0)
-print(f"Movie ID: {movie_id}")
-print(f"Title: {title}")
-print(f"Released Year: {released_year}")
-print(f"Runtime: {runtime}")
-print(f"Genre: {genre}")
-print(f"IMDB Rating: {imdb_rating}")
-print(f"Director: {director}")
-print("")
+    movie_id, title, released_year, runtime, genre, imdb_rating, director = training_set.__getitem__(0)
+    print(f"Movie ID: {movie_id}")
+    print(f"Title: {title}")
+    print(f"Released Year: {released_year}")
+    print(f"Runtime: {runtime}")
+    print(f"Genre: {genre}")
+    print(f"IMDB Rating: {imdb_rating}")
+    print(f"Director: {director}")
+    print("")
 
-# batch_size (int, optional) – how many samples per batch to load (default: 1).
-training_loader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True)
-validation_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=False)
+    # batch_size (int, optional) – how many samples per batch to load (default: 1).
+    training_loader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True)
+    validation_loader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=False)
 
-# Report split sizes
-print('Training set has {} instances'.format(len(training_set)))
-print('Validation set has {} instances'.format(len(validation_set)))
+    # Report split sizes
+    print('Training set has {} instances'.format(len(training_set)))
+    print('Validation set has {} instances'.format(len(validation_set)))
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
 
-print(f"Using {device} device")
-print("")
+    print(f"Using {device} device")
+    print("")
 
-# Model
-model = NeuralNetwork().to(device)
+    # Model
+    model = NeuralNetwork().to(device)
 
-# Loss Function
-loss_fn = torch.nn.CrossEntropyLoss()
+    # Loss Function
+    loss_fn = torch.nn.CrossEntropyLoss()
 
-# Optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0005)
+    # Optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0005)
 
-# Training Loop
-timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-writer = SummaryWriter('runs/movie_trainer_{}'.format(timestamp))
-epoch_number = 0
-EPOCHS = 75
-best_vloss =1.4948114841678643*math.pow(10, 18)
+    # Training Loop
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter('runs/movie_trainer_{}'.format(timestamp))
+    epoch_number = 0
+    EPOCHS = 75
+    best_vloss =1.4948114841678643*math.pow(10, 18)
 
-# 1.4948114841678643e+18
+    # 1.4948114841678643e+18
 
-for epoch in range(EPOCHS):
-    print('EPOCH {}:'.format(epoch_number + 1))
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch_number + 1))
 
-    # Make sure gradient tracking is on, and do a pass over the data
-    model.train(True)
-    avg_loss = train_one_epoch(epoch_number, writer)
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(epoch_number, writer, training_loader, optimizer, device, model, loss_fn)
 
-    running_vloss = 0.0
+        running_vloss = 0.0
 
-    # Set model to evaluation mode
-    model.eval()
+        # Set model to evaluation mode
+        model.eval()
 
-    # Disable gradient computation and reduce memory consumption
-    with torch.no_grad():
-        for i, vdata in enumerate(validation_loader):
-            vmovie_id, vtitle, vreleased_year, vruntime, vgenre, vimdb_rating, vdirector = vdata
+        # Disable gradient computation and reduce memory consumption
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vmovie_id, vtitle, vreleased_year, vruntime, vgenre, vimdb_rating, vdirector = vdata
 
-            vmovie_id = vmovie_id.to(device)
-            vmovie_id = vmovie_id.type(torch.float32)
+                vmovie_id = vmovie_id.to(device)
+                vmovie_id = vmovie_id.type(torch.float32)
 
-            if vmovie_id.shape[0] != BATCH_SIZE:
-                continue
+                if vmovie_id.shape[0] != BATCH_SIZE:
+                    continue
 
-            voutputs = model(vmovie_id)
-            vloss = loss_fn(voutputs, vmovie_id)
-            running_vloss += vloss
+                voutputs = model(vmovie_id)
+                vloss = loss_fn(voutputs, vmovie_id)
+                running_vloss += vloss
 
-    print(f"running_vloss: {running_vloss}")
+        print(f"running_vloss: {running_vloss}")
 
-    avg_vloss = running_vloss / (i + 1)
-    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
 
-    # Log the running loss average per batch
-    # for both training and validation
-    writer.add_scalars('Training vs. Validation Loss', {'Training': avg_loss, 'Validation': avg_vloss}, epoch_number + 1)
-    writer.flush()
+        # Log the running loss average per batch
+        # for both training and validation
+        writer.add_scalars('Training vs. Validation Loss', {'Training': avg_loss, 'Validation': avg_vloss}, epoch_number + 1)
+        writer.flush()
 
-    # Track the best performance, and save the model's state
-    if avg_vloss <= best_vloss:
-        best_vloss = avg_vloss
-        model_path = TRAINED_MODEL_DIR + 'model_{}_{}.pt'.format(timestamp, epoch_number)
-        torch.save(model.state_dict(), model_path)
+        # Track the best performance, and save the model's state
+        if avg_vloss <= best_vloss:
+            best_vloss = avg_vloss
+            model_path = TRAINED_MODEL_DIR + 'model_{}_{}.pt'.format(timestamp, epoch_number)
+            torch.save(model.state_dict(), model_path)
 
-    epoch_number += 1
+        epoch_number += 1
+
+if __name__ == "__main__":
+    main()
