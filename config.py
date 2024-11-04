9cf73e438e8585b7955a12d14d4beebf19d39d5d
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-03 22:13:26 -0500

    the training program trains the model with the training movies dataset

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ CSV_TRAINING_DATASET = "training_dataset/training_movies.csv"
 CSV_VALIDATION_DATASET = "validation_dataset/validation_movies.csv"
 CSV_PERSONALIZED_DATASET = "personalized_dataset/recommended_movies.csv"
 PICKED_MOVIE_TEXT_FILE = "personalized_dataset/picked_movie.txt"
+BATCH_SIZE = 5
