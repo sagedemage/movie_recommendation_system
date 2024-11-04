e0cfb667f92a8636bb13c57b48266cb978543ad3
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-04 18:02:45 -0500

    simplified the comment

--- generate_training_validation_datasets.py
+++ generate_training_validation_datasets.py
@@ -10,8 +10,7 @@ from config import CSV_DATASET, CSV_TRAINING_DATASET, CSV_VALIDATION_DATASET
 
 
 def main():
-    # 1. Read CSV files
-    # Read the dataset
+    # 1. Read the dataset
     df_data = pd.read_csv(CSV_DATASET)
 
     # 2. Split the dataset into training and validation datasets
