4d7fd0128ec7f3e365cee78f30e7113a9b636e8a
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-04 17:42:16 -0500

    documented the config source file

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 """
-Config for setting the file path for the movie dataset and
-the validation dataset
+Config for:
+1. setting the file path for the CSV datasets
+2. setting the file path for the picked movie text file
+3. setting the batch size
 """
 
+# CSV datasets
 CSV_DATASET = "dataset/imdb_top_1000.csv"
 CSV_TRAINING_DATASET = "training_dataset/training_movies.csv"
 CSV_VALIDATION_DATASET = "validation_dataset/validation_movies.csv"
 CSV_PERSONALIZED_DATASET = "personalized_dataset/recommended_movies.csv"
+
+# The picked movie text file is used to remember what movie the user chose
 PICKED_MOVIE_TEXT_FILE = "personalized_dataset/picked_movie.txt"
+
+# The batch size for preparing data with DataLoaders
 BATCH_SIZE = 5
